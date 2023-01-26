eabc63ac6d9ffa49fd246272a0d28ffbd2cd9c71
FerDev2 <fer.dev21@gmail.com>
2023-01-26 13:10:09 -0500

    cambio 2

--- JS/index.js
+++ JS/index.js
@@ -2,6 +2,7 @@ const menu = document.getElementById("menu");
 const options = document.getElementById("options");
 const clo = document.createElement("li");
 const title = document.createElement("h5");
+const back = document.getElementById("back");
 
 
 clo.innerHTML= "X";
@@ -11,12 +12,13 @@ menu.addEventListener("click", () => {
     options.appendChild(clo);
     options.classList.toggle("header-options");
     options.classList.add("header-options--active");
+    back.style.display='block';
 })
 
 clo.addEventListener("click", () => {
     options.removeChild(clo);
     options.classList.toggle("header-options");
     options.classList.remove("header-options--active");
-
+    back.style.display='none';
 
 })
